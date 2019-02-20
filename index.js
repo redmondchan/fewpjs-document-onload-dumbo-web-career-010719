b5375135034c1fe21934a5383d05caea47df04a5
Redmond Chan <redmondchan92@gmail.com>
2019-02-19 20:02:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+function domContentLoaded(){
+  document.addEventListener("DOMContentLoaded", function(event) {
+    updateDOM()
+  })
+}
+
+function updateDOM(){
+  document.getElementById("text").textContent = "This is really cool!"
+}
